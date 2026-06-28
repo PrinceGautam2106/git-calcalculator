99cc818a9ff14218e899b6c0320e8d820f2f1917
princegautam <princegautam2106@gmail.com>
2026-06-28 16:55:47 +0530

    prompt changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://calaifitness.com',
+  site: process.env.SITE_URL || 'https://calaifitness.com',
   integrations: [tailwind()],
 });
